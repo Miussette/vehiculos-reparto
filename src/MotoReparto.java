999a92284054d1dc0690b8a50f2005499f92635a
Zuelem <chanillao.zuelem@gmail.com>
2026-04-13 20:07:32 -0400

    code:agregar codigo de motoreparto

--- src/MotoReparto.java
+++ src/MotoReparto.java
@@ -1,2 +1,28 @@
-public class MotoReparto {
+package modelo;
+
+public class Motoreparto extends Vehiculo {
+
+    private boolean cajaTermica;
+
+    // Constructor
+    public Motoreparto(String patente, String marca, String modelo, boolean disponible, boolean cajaTermica) {
+        super(patente, marca, modelo, disponible);
+        this.cajaTermica = cajaTermica;
+    }
+
+    // Getter
+    public boolean isCajaTermica() {
+        return cajaTermica;
+    }
+
+    // Setter
+    public void setCajaTermica(boolean cajaTermica) {
+        this.cajaTermica = cajaTermica;
+    }
+
+    // Polimorfismo
+    @Override
+    public void mostrarDetalle() {
+        System.out.println("Caja térmica: " + (cajaTermica ? "Sí" : "No"));
+    }
 }
