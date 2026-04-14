78b3028de3c28f7e6cb038c4f9a98a8e76a036e8
Miussette alfaro <108189604+Miussette@users.noreply.github.com>
2026-04-13 20:10:52 -0400

    Extend Furgon class from Vehiculo and add features

--- src/Furgon.java
+++ src/Furgon.java
@@ -1,2 +1,29 @@
-public class Furgon {
+public class Furgon extends Vehiculo {
+
+    private double volumenCarga;
+
+    public Furgon(String patente, String marca, String modelo, double capacidadCarga, double volumenCarga) {
+        super(patente, marca, modelo, capacidadCarga);
+        setVolumenCarga(volumenCarga);
+    }
+
+    // GETTER
+    public double getVolumenCarga() {
+        return volumenCarga;
+    }
+
+    // SETTER con validación
+    public void setVolumenCarga(double volumenCarga) {
+        if (volumenCarga <= 0) {
+            throw new IllegalArgumentException("Volumen debe ser > 0");
+        }
+        this.volumenCarga = volumenCarga;
+    }
+
+    // POLIMORFISMO
+    @Override
+    public void mostrarDetalle() {
+        super.mostrarDetalle();
+        System.out.println("Volumen: " + volumenCarga + " m³");
+    }
 }
