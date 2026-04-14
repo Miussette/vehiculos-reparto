9b44610b82086c983e1482fd56f8d07598788a51
Matías Flores <matfroig@gmail.com>
2026-04-13 20:19:30 -0400

    codigo: Se agrega clase main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void registrarCamion() {
+        System.out.println("(pendiente) Registrar camión");
+    }
+
+    public static void registrarFurgon() {
+        System.out.println("(pendiente) Registrar furgón");
+    }
+
+    public static void registrarMoto() {
+        System.out.println("(pendiente) Registrar moto");
+    }
+
+    public static void mostrarTodos() {
+        System.out.println("(pendiente) Registrar furgón");
+    }
+
+    public static void mostrarDisponibles() {
+        System.out.println("(pendiente) Vehículos Disponibles");
+    }
+
+    public static void marcarNoDisponible() {
+        System.out.println("(pendiente) I");
+    }
+
+    public static void mostrarReporte() {
+        System.out.println("(pendiente) Mostrar");
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Vehiculo> vehiculos = new ArrayList<>();
+
+        int opcion;
+
+        do {
+            System.out.println("=== SISTEMA DE VEHÍCULOS DE REPARTO ===");
+            System.out.println("1. Registrar camión");
+            System.out.println("2. Registrar furgón");
+            System.out.println("3. Registrar moto de reparto");
+            System.out.println("4. Mostrar todos los vehículos");
+            System.out.println("5. Mostrar vehículos disponibles");
+            System.out.println("6. Marcar vehículo como no disponible");
+            System.out.println("7. Mostrar reporte general");
+            System.out.println("8. Salir");
+
+            System.out.print("Ingrese una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1: registrarCamion();          break;
+                case 2: registrarFurgon();          break;
+                case 3: registrarMoto();            break;
+                case 4: mostrarTodos();             break;
+                case 5: mostrarDisponibles();       break;
+                case 6: marcarNoDisponible();       break;
+                case 7: mostrarReporte();           break;
+                case 8: System.out.println("¡Hasta luego!"); break;
+                default: System.out.println("Opción inválida.");
+            }
+
+        } while (opcion != 8);
+
+        scanner.close();
+
+
     }
 }
